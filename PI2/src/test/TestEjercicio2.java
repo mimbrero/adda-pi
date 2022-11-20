49171c0058667771ab689f9f7d7ce5a808e1461f
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-11-20 18:19:34 +0100

    refactor(PI2): make TestEjercicio2 use BenchmarkTest

--- PI2/src/test/TestEjercicio2.java
+++ PI2/src/test/TestEjercicio2.java
@@ -1,20 +1,21 @@
 package test;
 
-import java.io.IOException;
-import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 
 import ejercicios.Ejercicio2;
-import util.benchmark.Benchmark;
 import util.benchmark.RandomListPopulator;
-import util.curvefitting.GraficosAjuste;
 import util.curvefitting.TipoAjuste;
-import util.test.Test;
+import util.test.BenchmarkTest;
 
-public class TestEjercicio2 extends Test {
+public class TestEjercicio2 extends BenchmarkTest {
 
-	private static final String PATH_FORMAT = "ficheros/tiempos-quicksort-umbral-%d.csv";
+	private final Ejercicio2 ejercicio2 = new Ejercicio2();
+	private final Integer[] thresholds = {4, 25, 100, 500};
+
+	public TestEjercicio2() {
+		super("QuickSort", "ficheros/tiempos-quicksort-umbral-%s.csv");
+	}
 
 	@Override
 	public void test() {
@@ -25,45 +26,20 @@ public class TestEjercicio2 extends Test {
 	private void generateBenchmarks() {
 		RandomListPopulator populator = new RandomListPopulator();
 
-		this.generateBenchmark(size -> Ejercicio2.quickSort(populator.generate(size), 4), 4);
-		this.generateBenchmark(size -> Ejercicio2.quickSort(populator.generate(size), 25), 25);
-		this.generateBenchmark(size -> Ejercicio2.quickSort(populator.generate(size), 100), 100);
-		this.generateBenchmark(size -> Ejercicio2.quickSort(populator.generate(size), 500), 500);
-	}
-
-	private void generateGraphs() {
-		this.generateGraph(4, TipoAjuste.NLOGN_0);
-		this.generateGraph(25, TipoAjuste.NLOGN_0);
-		this.generateGraph(100, TipoAjuste.NLOGN_0);
-		this.generateGraph(500, TipoAjuste.NLOGN_0);
-		this.generateCombinedGraph();
-	}
-
-	private void generateBenchmark(Consumer<Integer> implementation, int fileName) {
-		Benchmark benchmark = new Benchmark(implementation, 50, 50000, 500, 5, 30);
-		benchmark.run();
-
-		try {
-			benchmark.writeResults(Path.of(PATH_FORMAT.formatted(fileName)));
-		} catch (IOException e) {
-			e.printStackTrace();
+		for (Integer threshold : this.thresholds) {
+			this.generateBenchmark(
+					size -> ejercicio2.quickSort(populator.generate(size), threshold), 
+					threshold.toString(), 50, 50000, 500, 5, 30);
 		}
 	}
 
-	private void generateGraph(Integer fileName, TipoAjuste tipoAjuste) {
-		GraficosAjuste.show(PATH_FORMAT.formatted(fileName), tipoAjuste, fileName.toString());
-	}
+	private void generateGraphs() {
+		for (Integer threshold : this.thresholds) {
+			this.generateGraph(threshold.toString(), TipoAjuste.NLOGN_0);
+		}
 
-	private void generateCombinedGraph() {
-		List<Integer> names = List.of(4, 25, 100, 500);
-
-		GraficosAjuste.showCombined(
-				"QuickSort", 
-				names.stream()
-					.map(fileName -> PATH_FORMAT.formatted(fileName))
-					.toList(),
-				names.stream().map(i -> i.toString()).toList()
-		);
+		List<String> fileNames = Arrays.stream(this.thresholds).map(threshold -> threshold.toString()).toList();
+		this.generateCombinedGraph(fileNames);
 	}
 
 	public static void main(String[] args) {
