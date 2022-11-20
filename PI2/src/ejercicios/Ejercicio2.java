356f032b2241e457c009c26bbafe035349bfe124
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-11-20 18:19:08 +0100

    feat(PI2): create Ejercicio2

--- PI2/src/ejercicios/Ejercicio2.java
+++ PI2/src/ejercicios/Ejercicio2.java
@@ -11,58 +11,51 @@ import us.lsi.math.Math2;
 public class Ejercicio2 {
 
 	@SuppressWarnings("unchecked")
-	public static <E> Integer quickSort(List<E> lista, E elem) {
-		return quickSort(lista, 0, lista.size(), (Comparator<? super E>) Comparator.naturalOrder(), 4);
+	public <E> void quickSort(List<E> lista, int umbral) {
+		this.quickSort(lista, 0, lista.size(), (Comparator<? super E>) Comparator.naturalOrder(), umbral);
 	}
 
-	private static <E> Integer quickSort(List<E> lista, int i, int j, Comparator<? super E> ord, Integer umbral) {
-		Integer r = -1;
-		Preconditions.checkArgument(j>=i);
+	private <E> void quickSort(List<E> lista, int i, int j, Comparator<? super E> ord, Integer umbral) {
+		Preconditions.checkArgument(j >= i);
 
-		while(r == -1) {
-			if(j-i <= umbral) {
-				ordenaBase(lista, i, j, ord);
-			} else {
-				E pivote = escogePivote(lista, i, j);
-				IntPair p = banderaHolandesa(lista, pivote, i, j, ord);
-				quickSort(lista,i,p.first(), ord, umbral);
-				quickSort(lista,p.second(), j, ord, umbral);            
-			}
+		if (j - i <= umbral)
+			this.ordenaBase(lista, i, j, ord);
+		else {
+			E pivote = this.escogePivote(lista, i, j);
+			IntPair p = this.banderaHolandesa(lista, pivote, i, j, ord);
+			this.quickSort(lista, i, p.first(), ord, umbral);
+			this.quickSort(lista, p.second(), j, ord, umbral);       
 		}
-
-		return r;
 	}
 
-	private static <E> IntPair banderaHolandesa(List<E> ls, E pivote, Integer i, Integer j,  Comparator<? super E> cmp){
+	private <E> IntPair banderaHolandesa(List<E> ls, E pivote, Integer i, Integer j, Comparator<? super E> cmp) {
 		Integer a=i, b=i, c=j;
-		while (c-b>0) {
-			E elem =  ls.get(b);
-			if (cmp.compare(elem, pivote)<0) {
-				List2.intercambia(ls,a,b);
+
+		while (c - b > 0) {
+			E elem = ls.get(b);
+
+			if (cmp.compare(elem, pivote) < 0) {
+				List2.intercambia(ls, a, b);
 				a++;
 				b++;
-			} else if (cmp.compare(elem, pivote)>0) {
-				List2.intercambia(ls,b,c-1);
+			} else if (cmp.compare(elem, pivote) > 0) {
+				List2.intercambia(ls, b, c-1);
 				c--;    
-			} else {
+			} else
 				b++;
-			}
 		}
+
 		return IntPair.of(a, b);
 	}
 
-	private static <E> E escogePivote(List<E> lista, int i, int j) {
-		E pivote = lista.get(Math2.getEnteroAleatorio(i, j));
-		return pivote;
+	private <E> E escogePivote(List<E> lista, int i, int j) {
+		return lista.get(Math2.getEnteroAleatorio(i, j));
 	}
 
-	private static <T> void ordenaBase(List<T> lista, Integer inf, Integer sup, Comparator<? super T> ord) {        
-		for (int i = inf; i < sup; i++) {
-			for(int j = i+1; j < sup; j++){
-				if(ord.compare(lista.get(i),lista.get(j))>0){
+	private <T> void ordenaBase(List<T> lista, Integer inf, Integer sup, Comparator<? super T> ord) {
+		for (int i = inf; i < sup; i++)
+			for(int j = i+1; j < sup; j++)
+				if(ord.compare(lista.get(i),lista.get(j))>0)
 					List2.intercambia(lista, i, j);
-				}
-			}
-		}
 	}
 }
