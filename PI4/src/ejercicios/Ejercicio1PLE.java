399d194b90f693974bcb275b362b92339375d6d8
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2023-03-18 16:48:03 +0100

    feat(PI4): run for all data files

--- PI4/src/ejercicios/Ejercicio1PLE.java
+++ PI4/src/ejercicios/Ejercicio1PLE.java
@@ -10,37 +10,44 @@ import us.lsi.gurobi.GurobiSolution;
 import us.lsi.solve.AuxGrammar;
 
 public class Ejercicio1PLE {
-	
+
 	private static CoffeeData coffeeData;
 
 	public static Integer getTypesNumber() {
 		return coffeeData.getTypesNumber();
 	}
-	
+
 	public static Integer getVarietiesNumber() {
 		return coffeeData.getVarietiesNumber();
 	}
-	
+
 	public static Integer getVarietyProfit(Integer i) {
 		return coffeeData.getVarietyProfit(i);
 	}
-	
+
 	public static Integer getTypeQuantity(Integer j) {
 		return coffeeData.getTypeQuantity(j);
 	}
-	
+
 	public static Double getTypePercentage(Integer i, Integer j) {
 		return coffeeData.getTypePercentage(i, j);
 	}
-	
-	public static void main(String[] args) throws IOException {
-		Locale.setDefault(Locale.US);
-		
-		coffeeData = CoffeeData.fromFile(Path.of("ficheros/Ejercicio1DatosEntrada1.txt"));
-		
-		AuxGrammar.generate(Ejercicio1PLE.class, "lsi-models/ejercicio1.lsi", "lp-models/ejercicio1.lp");
-		GurobiSolution solution = GurobiLp.gurobi("lp-models/ejercicio1.lp");
+
+	public static void run(int dataFileNumber) throws IOException {
+		Path dataFilePath = Path.of("ficheros/Ejercicio1DatosEntrada%d.txt".formatted(dataFileNumber));
+		String lpFilePath = "lp-models/ejercicio1-%d.lp".formatted(dataFileNumber);
+
+		coffeeData = CoffeeData.fromFile(dataFilePath);
+
+		AuxGrammar.generate(Ejercicio1PLE.class, "lsi-models/ejercicio1.lsi", lpFilePath);
+		GurobiSolution solution = GurobiLp.gurobi(lpFilePath);
 		System.out.println(solution.toString((s, d) -> d > 0.));
 	}
 
+	public static void main(String[] args) throws IOException {
+		Locale.setDefault(Locale.US);
+
+		for (int i = 1; i <= 3; i++)
+			run(i);
+	}
 }
